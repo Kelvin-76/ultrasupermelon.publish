55d679d9e7bf271e2896c2a030aa8d66d1dc8ee9
Kelvin-76 <33400764+Kelvin-76@users.noreply.github.com>
2022-04-04 23:49:51 +0800

    init

---
+++ js/general.js
@@ -0,0 +1,15 @@
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+$(document).ready(function () {
+    $("body").click(function () {
+        $('.navbar-collapse').collapse('hide');
+    });
+});
